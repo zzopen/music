1279c050ad0baf75a4bfef94d2b697633d846bd3
许磊 <xulei131401@outlook.com>
2023-07-11 00:04:29 +0800

    fix:暂存

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git add . && git commit -m 'fix:暂存'
+git add . && git commit -m 'fix:暂存' && git push origin main
 # GIT_BIN=$(which git)
 # $("${GIT_BIN} add . " && \ "${GIT_BIN} commit -m 'fix: 暂存'" && \ "${GIT_BIN} push origin main")
